e1c2169e2365db339f9bcca0da4968b6d53844f1
Mason <nerdinacan@gmail.com>
2019-06-20 11:10:22 -0700

    added hda triggers

--- lib/galaxy/model/migrate/versions/0155_add_content_update_time.py
+++ lib/galaxy/model/migrate/versions/0155_add_content_update_time.py
@@ -16,7 +16,7 @@ def upgrade(migrate_engine):
     print(__doc__)
     metadata.bind = migrate_engine
     metadata.reflect()
-    drop_update_trigger(migrate_engine)
+    # drop_update_trigger(migrate_engine)
     install_update_trigger(migrate_engine)
 
 
@@ -34,6 +34,19 @@ def install_update_trigger(migrate_engine):
 
     if migrate_engine.name in ['postgres', 'postgresql']:
         pg_create_trigger = DDL("""
+            
+            CREATE FUNCTION update_hda_update_time()
+                RETURNS trigger
+                LANGUAGE 'plpgsql'
+            AS $BODY$
+            BEGIN
+                UPDATE history_dataset_association hda
+                SET update_time = current_timestamp
+                WHERE hda.dataset_id = NEW.id;
+                RETURN NEW;
+            END;
+            $BODY$;
+
             CREATE FUNCTION update_history_update_time()
                 RETURNS trigger
                 LANGUAGE 'plpgsql'
@@ -41,15 +54,20 @@ def install_update_trigger(migrate_engine):
             BEGIN
                 UPDATE history h
                 SET update_time = current_timestamp
-                FROM history_dataset_association hda
-                WHERE h.id = hda.history_id
-                AND hda.dataset_id = NEW.id;
+                WHERE h.id = NEW.history_id;
                 RETURN NEW;
             END;
             $BODY$;
+
             CREATE TRIGGER trigger_dataset_aidur
                 AFTER INSERT OR DELETE OR UPDATE
                 ON dataset
+                FOR EACH ROW
+                EXECUTE PROCEDURE update_hda_update_time();
+
+            CREATE TRIGGER trigger_dataset_aidur
+                AFTER INSERT OR DELETE OR UPDATE
+                ON history_dataset_association
                 FOR EACH ROW
                 EXECUTE PROCEDURE update_history_update_time();
         """)
@@ -67,7 +85,9 @@ def drop_update_trigger(migrate_engine):
 
     if migrate_engine.name in ['postgres', 'postgresql']:
         pg_drop_trigger = DDL("""
-            DROP TRIGGER IF EXISTS trigger_dataset_aidur ON dataset;
+            DROP TRIGGER IF EXISTS update_hda_update_time ON dataset;
+            DROP TRIGGER IF EXISTS update_history_update_time ON history_dataset_association;
+            DROP FUNCTION IF EXISTS update_hda_update_time();
             DROP FUNCTION IF EXISTS update_history_update_time();
         """)
         pg_drop_trigger.execute(bind=migrate_engine)
@@ -79,18 +99,25 @@ def drop_update_trigger(migrate_engine):
 
 def build_trigger(op):
     create_trigger_template = """
+
         CREATE TRIGGER trigger_dataset_a{op_initial}r
             AFTER {operation}
             ON dataset
             FOR EACH ROW
             BEGIN
+                UPDATE history_dataset_association
+                SET update_time = current_timestamp
+                WHERE dataset_id = {rowset}.id;
+            END;
+
+        CREATE TRIGGER trigger_hda_a{op_initial}r
+            AFTER {operation}
+            ON history_dataset_association
+            FOR EACH ROW
+            BEGIN
                 UPDATE history
                 SET update_time = current_timestamp
-                WHERE id IN (
-                    SELECT hda.history_id
-                    FROM history_dataset_association AS hda
-                    WHERE hda.dataset_id = {rowset}.id
-                );
+                WHERE id = {rowset}.history_id;
             END;
     """
     rs = 'OLD' if op == 'DELETE' else 'NEW'
@@ -99,6 +126,9 @@ def build_trigger(op):
 
 
 def build_drop_trigger(op):
-    drop_template = 'DROP TRIGGER IF EXISTS trigger_dataset_a{op_initial}r;'
+    drop_template = """
+        DROP TRIGGER IF EXISTS trigger_dataset_a{op_initial}r;
+        DROP TRIGGER IF EXISTS trigger_hda_a{op_initial}r;
+    """
     sql = drop_template.format(op_initial=op.lower()[0])
     return DDL(sql)
